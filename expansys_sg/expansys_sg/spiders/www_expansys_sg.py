526329f8f07ad350219d31d8c01bb65f44228157
Miguel Concepcion <miguel.concepcion@outlook.com>
2015-12-03 14:34:59 +0800

    Accessories are now scraped. some price and availability still not working,

--- expansys_sg/expansys_sg/spiders/www_expansys_sg.py
+++ expansys_sg/expansys_sg/spiders/www_expansys_sg.py
@@ -4,13 +4,16 @@ from expansys_sg.items import ExpansysItem
 import re
 
 #init
-class DmozSpider(scrapy.Spider):
+class ExpansysSpider(scrapy.Spider):
     name = "expansys"
     allowed_domains = ["www.expansys.com.sg"]
     start_urls = [
-        "http://www.expansys.com.sg/"]
+       "http://www.expansys.com.sg/mobile-phones/phone-accessories/","http://www.expansys.com.sg/tablet-pcs+ipads/tablet-accessories/","http://www.expansys.com.sg/action/cameras/compact-camera-accessories/","http://www.expansys.com.sg/computing/computer-hardware+accessories/"]
 
     def parse(self, response):
+        for ur in self.start_urls:
+            yield scrapy.Request(ur, callback=self.parse_dir_contents)
+
         for href in response.css("ul.asia.me > li > a::attr('href')"):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url, callback=self.parse_dir_contents)
@@ -24,28 +27,30 @@ class DmozSpider(scrapy.Spider):
         #gfcg
         for sel in response.xpath('//li[@class="title"]//a/@href'):
             url = response.urljoin(sel.extract())
-           # print url+' Grid'
+            print url
             yield scrapy.Request(url, callback=self.parse_grid_contents)
         for href in response.xpath('//a[@class="next"]/@href'):
             url = response.urljoin(href.extract())
             print url+ "next pager"
             yield scrapy.Request(url,callback = self.parse_dir_contents)
-
+    lnkk=[]
     def parse_grid_contents(self,response):
         for r in response.xpath('//html'):
              
             item = ExpansysItem()
             #print "got in here"
-            item['title'] = r.xpath('//h1[@itemprop="name"]/text()').extract()
-            item['link'] =  r.xpath('//link/@href')[0].extract()
-            item['price'] = r.xpath('//span[@itemprop="price"]/text()').extract()
-            item['currency'] = r.xpath('//p[@id="price"]/meta/@content').extract()
-            m = re.search(r'sku:(\d+)',r.xpath('//ul[@class="product-sku"]/li/span/@content')[0].extract())
-            item['sku']=m.group(0)
-            m = re.search(r'ean:(\d+)',r.xpath('//ul[@class="product-sku"]/li/span/@content')[0].extract())
-            item['ean'] = m.group(0)
-            item['primary_image_url']=r.xpath('//a[@class ="js-primary-image-link"]/@href').extract()
-            item['time'] = datetime.datetime.now().time()
+            if r.xpath('//link/@href')[0].extract() not in self.lnkk:
+                item['title'] = r.xpath('//h1[@itemprop="name"]/text()').extract()
+                item['link'] =  r.xpath('//link/@href')[0].extract()
+                item['price'] = r.xpath('//span[@itemprop="price"]/text()').extract()
+                item['currency'] = r.xpath('//p[@id="price"]/meta/@content').extract()
+                m = re.search(r'sku:(\d+)',r.xpath('//ul[@class="product-sku"]/li/span/@content')[0].extract())
+                item['sku']=m.group(0)
+                n = re.search(r'ean:(\d+)',r.xpath('//ul[@class="product-sku"]/li/span/@content')[0].extract())
+                item['ean'] = n
+                item['primary_image_url']=r.xpath('//a[@class ="js-primary-image-link"]/@href').extract()
+                item['time'] = datetime.datetime.now().time()
+                self.lnkk.append(r.xpath('//link/@href')[0].extract())
             
             item['category'] =r.xpath('//ul[@id="breadcrumbs"]//span/text()').extract()
             #item['desc']=r.xpath('//div[@class="what_we_say"]/text()').extract()
